d590b77d0ce6bf6bae190fd5edcb14ef0befe28d
mahan zandi <zandim221@gmail.com>
2026-02-06 18:26:52 +0330

    added some metadata & favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,14 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "MVP Chatbot",
-  description: "An MVP chatbot with a local OpenAI-compatible backend"
+  description: "An MVP chatbot with a local Ollama Api",
+  icons: {
+    icon: "/favicon.jpg"
+  },
+  authors: [{
+    name: "mahan zandi",
+    url: "https://www.mahanzandi.ir/"
+  }]
 };
 
 export default function RootLayout({
